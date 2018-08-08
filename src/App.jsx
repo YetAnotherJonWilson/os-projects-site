6a93c990b2046d9fc00e8efee2f2b345ba64f27c
Jon Wilson <jonwilsoncomedy@gmail.com>
2018-08-07 22:49:32 -0500

    adding src folder and app file

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>List of Projects and Ideas</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
